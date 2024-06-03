7cca59e1fd6ba02523a20286eebff70cc35fb8a2
vinonsan9744 <asp2020131@as.rjt.ac.lk>
2024-06-03 11:14:43 +0530

    vino

--- backend/models/administrativeOfficerModel.js
+++ backend/models/administrativeOfficerModel.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-const { Schema } = mongoose;
-
 const crypto = require('crypto');
 
-function generatePassword(length = 16) {
+const { Schema } = mongoose;
+
+function generatePassword(length = 8) {
   const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=';
   let password = '';
   const bytes = crypto.randomBytes(length);
@@ -13,53 +13,50 @@ function generatePassword(length = 16) {
   return password;
 }
 
-
-
-
 const administrativeOfficerSchema = new mongoose.Schema({
-    AD_ID: {
-        type: String,
-        unique: true,
-        match: [/^AID\d{3}$/, 'AD_ID must match the format "AD###"']
-    },
-    Name: {
-        type: String,
-        required: true
-    },
-    Email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.']
-    },
-    Password: {
-        type: String,
-        require : true
-
-    }
-    
+  AD_ID: {
+    type: String,
+    unique: true,
+    match: [/^AID\d{3}$/, 'AD_ID must match the format "AD###"']
+  },
+  Name: {
+    type: String,
+    required: true
+  },
+  Email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.']
+  },
+  Password: {
+    type: String,
+    required: false
+  }
 }, { timestamps: true });
-
-
 
 // Function to generate AD_ID before saving
 administrativeOfficerSchema.statics.generateAD_ID = async function() {
-    const count = await this.countDocuments({});
-    return `AID${(count + 1).toString().padStart(3, '0')}`;
+  const count = await this.countDocuments({});
+  return `AID${(count + 1).toString().padStart(3, '0')}`;
 };
 
 // Pre-save hook to generate AD_ID before saving
 administrativeOfficerSchema.pre('save', async function(next) {
-    if (!this.AD_ID) {
-        try {
-            this.AD_ID = await this.constructor.generateAD_ID();
-            next();
-        } catch (err) {
-            next(err);
-        }
-    } else {
-        return next();
+  if (!this.AD_ID) {
+    try {
+      this.AD_ID = await this.constructor.generateAD_ID();
+    } catch (err) {
+      return next(err);
     }
+  }
+
+  // Generate a password if not provided
+  if (!this.Password) {
+    this.Password = generatePassword();
+  }
+
+  next();
 });
 
 const AdministrativeOfficer = mongoose.model('AdministrativeOfficer', administrativeOfficerSchema);
