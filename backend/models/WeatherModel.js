f0d70008046f9ed082277fcb5a96f53f33760d64
unknown <jesumathu11@gmail.com>
2024-06-13 22:57:09 -0700

    last updated in backend

--- backend/models/WeatherModel.js
+++ backend/models/WeatherModel.js
@@ -46,7 +46,16 @@ WeatherSchema.statics.generateWeatherID = async function() {
     const count = await this.countDocuments({});
     return `WID${(count + 1).toString().padStart(3, '0')}`;
   };
-  
+  // Pre-save hook to generate AD_ID before saving
+  WeatherSchema.pre('save', async function(next) {
+    if (!this.WeatherID) {
+      try {
+        this.WeatherID = await this.constructor.generateWeatherID();
+      } catch (err) {
+        return next(err);
+      }
+    }
+});
 // Pre-save hook to generate WeatherID and set next_location before saving
 WeatherSchema.pre('save', async function(next) {
     if (!this.WeatherID) {
@@ -61,7 +70,7 @@ WeatherSchema.pre('save', async function(next) {
         try {
             const currentLocation = await mongoose.model('Location').findOne({ locationName: this.current_location });
             if (!currentLocation) {
-                return next(new Error('Current location not found vino'));
+                return next(new Error('Current location not found'));
             }
 
             const nextLocation = await mongoose.model('Location').findOne({ 
